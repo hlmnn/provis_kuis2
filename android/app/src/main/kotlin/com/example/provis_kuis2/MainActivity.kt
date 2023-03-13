3c55db024bca52b88df857459b724e17c5449cd3
Hilman Fauzi Herdiana <kk726627@gmail.com>
2023-03-13 07:41:07 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/provis_kuis2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.provis_kuis2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+ runApp(const MyApp());
+}
+
+
+class MyApp extends StatefulWidget {
+ const MyApp({Key? key}) : super(key: key);
+ @override
+ MyAppState createState() {
+   return MyAppState();
+ }
+}
+
+
+class MyAppState extends State<MyApp> {
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Hello App',
+     home: Scaffold(
+       appBar: AppBar(
+           leading: FlutterLogo(),
+           backgroundColor: Colors.blueGrey,
+           title: Text('Quiz Flutter'),
+           actions: <Widget>[ButtonNamaKelompok(), ButtonPerjanjian()]),
+       body: Center(
+         child: Text('Hello World!'),
+       ),
+     ),
+   );
+ }
+}
+
+
+class ButtonNamaKelompok extends StatelessWidget {
+ const ButtonNamaKelompok({Key? key}) : super(key: key);
+
+
+ @override
+ Widget build(BuildContext context) {
+   return IconButton(
+     icon: Icon(Icons.account_circle_rounded),
+     onPressed: () {
+       // icon account di tap
+       showDialog<String>(
+         context: context,
+         builder: (BuildContext context) => AlertDialog(
+           title: const Text('Kelompok 8'),
+           content: const Text('Rizal Teddyansyah (rizal17@upi.edu) ; Hilman Fauzi Herdiana (hilmanfauzih@upi.edu)'),
+           actions: <Widget>[
+             TextButton(
+               onPressed: () => Navigator.pop(context, 'OK'),
+               child: const Text('OK'),
+             ),
+           ],
+         ),
+       );
+     },
+   );
+ }
+}
+
+
+class ButtonPerjanjian extends StatelessWidget {
+ const ButtonPerjanjian({Key? key}) : super(key: key);
+
+
+ @override
+ Widget build(BuildContext context) {
+   return IconButton(
+     icon: Icon(Icons.access_alarm_rounded),
+     onPressed: () {
+       // icon setting ditap
+       const snackBar = SnackBar(
+         duration: Duration(seconds: 20),
+         content: Text('Kami berjanji  tidak akan berbuat curang dan atau membantu kelompok lain berbuat curang'),
+       );
+       ScaffoldMessenger.of(context).showSnackBar(snackBar);
+     },
+   );
+ }
+}
+
+
+
+
+
