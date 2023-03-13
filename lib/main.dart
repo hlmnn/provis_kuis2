8e1056a663ae82c2fc7d8db2c19b06cc48c02516
Hilman Fauzi Herdiana <kk726627@gmail.com>
2023-03-13 09:31:08 +0700

    commit main

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,153 @@ class MyApp extends StatefulWidget {
 
 
 class MyAppState extends State<MyApp> {
+  String pilihanMobil	= "Brio";  // harus diletakkan di sini 
+  String image = "Images/brio.jpg";
+
  @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'Hello App',
-     home: Scaffold(
-       appBar: AppBar(
-           leading: FlutterLogo(),
-           backgroundColor: Colors.blueGrey,
-           title: Text('Quiz Flutter'),
-           actions: <Widget>[ButtonNamaKelompok(), ButtonPerjanjian()]),
-       body: Center(
-         child: Text('Hello World!'),
-       ),
-     ),
-   );
- }
+  Widget build(BuildContext context) {
+    List<DropdownMenuItem<String>> mobil = [];
+    var itm1 = const DropdownMenuItem<String>(
+        value: "Brio",
+        child: Text("Brio"),
+    );
+    var itm2 = const DropdownMenuItem<String>(
+        value: "Civic",
+        child: Text("Civic"),
+    );
+    mobil.add(itm1);
+    mobil.add(itm2);
+
+
+    return MaterialApp(
+      title: 'Hello App',
+      home: Scaffold(
+        appBar: AppBar(
+            leading: FlutterLogo(),
+            backgroundColor: Colors.blueGrey,
+            title: Text('Quiz Flutter'),
+            actions: const <Widget>[ButtonNamaKelompok(), ButtonPerjanjian()]),
+        body: Center(
+          child: Column(         
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(20),
+                          child: Text(
+                            "Mobil Saya",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                        
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Image.asset(
+                            image,
+                            height: 100,
+                          ),
+                        ),
+
+                        ElevatedButton(
+                          onPressed: () {}, 
+                          child: const Text('Detail')
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: DropdownButton (
+                            value: pilihanMobil,
+                            items: mobil,
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                  if (newValue != null) {
+                                      pilihanMobil = newValue;
+                                      if (pilihanMobil == 'Brio') {
+                                        image = "Images/brio.jpg";
+                                      } else {
+                                        image = "Images/civic.jpg";
+                                      }
+                                  }
+                              });
+                            },
+                          ),
+                        ),
+                        
+                        RichText(
+                          text: TextSpan(children: [
+                            TextSpan(
+                              text: '$pilihanMobil\n',
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              )
+                            ),
+                            const TextSpan(text: 'Nomor Rangka: XXXXXXXX\n'),
+                            const TextSpan(text: 'Nomor Mesin: XXXXXXXX\n'),
+                          ]),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              
+              Container(
+                decoration: BoxDecoration(border: Border.all()),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: const [
+                        Text('Katalog Produk'),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                            padding: const EdgeInsets.all(14),
+                            child: Image.asset(
+                              "Images/brv.png",
+                              height: 50,
+                            ),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.all(14),
+                            child: Image.asset(
+                              "Images/accord.png",
+                              height: 80,
+                            ),
+                        ),
+                        Container(
+                            padding: EdgeInsets.all(14),
+                            child: Image.asset(
+                              "Images/mobilio.png",
+                              height: 80,
+                            ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 
@@ -49,7 +180,7 @@ class ButtonNamaKelompok extends StatelessWidget {
          context: context,
          builder: (BuildContext context) => AlertDialog(
            title: const Text('Kelompok 8'),
-           content: const Text('Rizal Teddyansyah (rizal17@upi.edu) ; Hilman Fauzi Herdiana (hilmanfauzih@upi.edu)'),
+           content: const Text('Rizal Teddyansyah (rizal17@upi.edu) ;\nHilman Fauzi Herdiana (hilmanfauzih@upi.edu)'),
            actions: <Widget>[
              TextButton(
                onPressed: () => Navigator.pop(context, 'OK'),
